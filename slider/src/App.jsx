605f2303212afbe0230d68ae0aa3ecaa5674c018
alicankocman <kocmanalican@gmail.com>
2024-04-15 14:26:37 +0300

    Multiple Items Slider Project Added

---
+++ slider/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './App.css'
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import MultipleItems from './Slider'
+
+function App() {
+
+  return (
+    <>
+    <h1 style={{position:"relative",bottom:"100px"}}>Multiple Items Slider</h1>
+<MultipleItems/>
+    </>
+  )
+}
+
+export default App
